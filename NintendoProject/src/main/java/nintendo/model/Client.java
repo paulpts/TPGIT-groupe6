a9f9e504702f65f1897d00c29cc93017adf2324f
paulpts <p.pontois@outlook.com>
2025-10-09 10:40:30 +0200

    étape 4 et 5
    ajout classe Achat, modif Client(list achats), modif main

--- NintendoProject/src/main/java/nintendo/model/Client.java
+++ NintendoProject/src/main/java/nintendo/model/Client.java
@@ -1,36 +1,60 @@
 package nintendo.model;
 
+import java.util.List;
+
 public class Client {
 	private String nom;
 	private String prenom;
+	private List<Achat> achats;
 	
 	
 	// Constructeur
-	public Client(String nom, String prenom) {
-		super();
+	
+
+	public Client(String nom, String prenom, List<Achat> achats) {
 		this.nom = nom;
 		this.prenom = prenom;
+		this.achats = achats;
 	}
-	
-	// getters et setters 
+
+
 	public String getNom() {
 		return nom;
 	}
+
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
+
 	public String getPrenom() {
 		return prenom;
 	}
+
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-	
-	// toString
+
+
+	public List<Achat> getAchats() {
+		return achats;
+	}
+
+
+	public void setAchats(List<Achat> achats) {
+		this.achats = achats;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", achats=" + achats + "]";
 	}
 	
+	
+	
+
 	
 }
