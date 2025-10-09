08b240b38a4a5a7f916f35f49eba71be3283dbdd
mplichon <plichon.matteo@gmail.com>
2025-10-09 10:30:22 +0200

    ajout de 2 attributs dans Console et ses classes filles

--- NintendoProject/src/main/java/nintendo/model/Portable.java
+++ NintendoProject/src/main/java/nintendo/model/Portable.java
@@ -1,9 +1,11 @@
 package nintendo.model;
 
+import java.time.LocalDate;
+
 public class Portable extends Console {
 
-	public Portable(String nom) {
-		super(nom);
+	public Portable(String nom, double prix, LocalDate dateSortie) {
+		super(nom, prix, dateSortie);
 	}
 
 }
